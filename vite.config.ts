d98901c6761d4dad247c342506ca7b3112cae967
ramirolacci <ramiroalejandrolacci19@gmail.com>
2025-09-16 16:11:37 -0300

    more fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
